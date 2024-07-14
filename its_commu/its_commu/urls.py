580cdc797e246b09f29a4e09c97a0a152508c1d1
sooyeonJoo <netwizjoo@gmail.com>
2024-07-14 12:45:08 +0900

    [20240714, 주수연, ADD] login_modal 추가

--- its_commu/its_commu/urls.py
+++ its_commu/its_commu/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
@@ -25,5 +25,5 @@ urlpatterns = [
     path('board/', TemplateView.as_view(template_name='board.html'), name='board'),
     path('my/', TemplateView.as_view(template_name='my.html'), name='my'),
     path('recruit/', TemplateView.as_view(template_name='recruit.html'), name='recruit'),
-    
+    path('account/', include('django.contrib.auth.urls'))
 ]
