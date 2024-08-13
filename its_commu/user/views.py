1e26d9d4330143d39839d6735c508599c209511f
jungseo813 <jungseo813@gmail.com>
2024-08-13 18:29:32 +0900

    [240813,윤정서,Add]로그인,로그아웃 구현

--- its_commu/user/views.py
+++ its_commu/user/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import auth
+
 
 # Create your views here.
 
@@ -7,3 +9,23 @@ def my_setting(request):
 
 def join(request):
     return render(request, 'join.html')
+
+#로그인
+def login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(
+            request, username=username, password=password
+        )
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, "login_error.html")
+    else:
+        return render(request, "base.html")
+
+
