e1743aca682871d9731fbf0f47730ee88412acb5
MinjuPark14 <vhtnsdl2004@naver.com>
2024-08-14 13:51:10 +0900

    [0814,minju,Add]

--- its_commu/user/models.py
+++ its_commu/user/models.py
@@ -1,10 +1,51 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 # Create your models here.
+
+class MyAccountManager(BaseUserManager):
+    # 일반 user 생성, id 가 userID를 의미함
+    def create_user(self, id, username, std_id, birth, tel, email, github, password=None):
+        if not id:
+            raise ValueError("Users must have an userID.")
+        if not username:
+            raise ValueError("Users must have an name.")
+        user = self.model(
+            id=id,
+            username=username,
+            std_id=std_id,
+            birth=birth,
+            tel=tel,
+            email=self.normalize_email(email),
+            github=github
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    # 관리자 User 생성
+    def create_superuser(self, id, username, std_id, birth, tel, email, github, password, **extra_fields):
+        user = self.create_user(
+            id=id,
+            username=username,
+            std_id=std_id,
+            birth=birth,
+            tel=tel,
+            email=email,
+            github=github,
+            password=password
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.is_active = True
+        user.save(using=self._db)
+        return user
+
+
 class User(models.Model):
     id = models.CharField(max_length=20, null=False)
     pw = models.CharField(max_length=255, null=False)
-    name = models.CharField(max_length=50, null=False, unique=True)
+    username = models.CharField(max_length=50, null=False, unique=True)
     std_id = models.CharField(max_length=20, primary_key=True, null=False)
     birth = models.DateField(null=False)
     tel = models.CharField(max_length=50, null=False)
@@ -16,6 +57,22 @@ class User(models.Model):
     insert_date = models.DateField(auto_now_add=True)
     insert_user = models.CharField(max_length=50)
     deleted = models.CharField(max_length=1)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    object = MyAccountManager()  # 헬퍼 클래스 사용
+
+    ID_FIELD = 'id'  # 로그인 ID로 사용할 필드
+    REQUIRED_FIELDS = ['username', 'std_id', 'birth', 'tel', 'email', 'github']  # 필수 작성 필드
 
     def __str__(self):
         return self.id
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_lable):
+        return True
+
--- its_commu/user/views.py
+++ its_commu/user/views.py
@@ -1,9 +1,39 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+
+from .models import *
+from .forms import UserForm, UserChangeForm
+from django.contrib.auth.decorators import login_required
+from django.conf import settings
+from django.contrib.auth.forms import PasswordResetForm
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import send_mail, BadHeaderError
+from django.db.models.query_utils import Q
+from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
 
 # Create your views here.
 
 def my_setting(request):
     return render(request, 'my_setting.html')
 
+
 def join(request):
-    return render(request, 'join.html')
+    if request.method == "POST":
+        print(request.POST)
+        form = UserForm(request.POST, request.FILES)
+        if form.is_valid():
+            account = form.save(commit=False)
+            username = form.cleaned_data.get('username')
+            email = form.cleaned_data.get('email')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, email=email, password=raw_password)
+            if user is not None:
+                login(request, user)
+            account.save()
+            return redirect('account:login')
+    else:
+        form = UserForm()
+    return render(request, 'join.html', {'form': form})
